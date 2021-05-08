f0e8a8e8cc196fa65f2c011356768d1986ec2aa7
Lucas Monteiro <lucas.monteiro58@gmail.com>
2021-05-08 11:42:50 -0300

    preload audio

--- src/plugins/preload.js
+++ src/plugins/preload.js
@@ -1,13 +1,17 @@
 'use strict'
 
 const path = require('@/assets/spritesheet.png').default
+const audioInit = require('@/assets/audios/inicioloop2.wav')
+
+console.log(audioInit)
+
 // const path = null
 
 function runBlock(context) {
   if (!context.isDev)
     window.onload = () => {
       const image = new Image()
-      image.onload = isAssetReady
+      image.onload = carregarAudio
       image.src = path
     }
   else {
@@ -16,9 +20,18 @@ function runBlock(context) {
   return false
 }
 function isAssetReady() {
+  console.log('carregou imagem')
   setTimeout(() => {
     document.querySelector('.loading-screen').classList.add('not-ready')
+    console.log('terminou timout')
   }, 2400)
+}
+
+function carregarAudio() {
+  console.log('carregou audio')
+  const audio = new Audio()
+  audio.onload = isAssetReady
+  audio.src = audioInit
 }
 
 export default runBlock
