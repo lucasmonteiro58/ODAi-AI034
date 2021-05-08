f35ce23c8ac00bec216d86716367ebb2b0ca8be3
Lucas Monteiro <lucas.monteiro58@gmail.com>
2021-05-08 11:30:33 -0300

    atualizando preload

--- src/plugins/preload.js
+++ src/plugins/preload.js
@@ -6,7 +6,6 @@ const path = require('@/assets/spritesheet.png').default
 function runBlock(context) {
   if (!context.isDev)
     window.onload = () => {
-      document.querySelector('.stage-container').classList.add('not-ready')
       const image = new Image()
       image.onload = isAssetReady
       image.src = path
@@ -18,7 +17,6 @@ function runBlock(context) {
 }
 function isAssetReady() {
   setTimeout(() => {
-    document.querySelector('.stage-container').classList.remove('not-ready')
     document.querySelector('.loading-screen').classList.add('not-ready')
   }, 2400)
 }
